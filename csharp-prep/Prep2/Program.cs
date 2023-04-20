e531bd3eae011a25ee0d7f0851ac87bca157dddd
Emrys241 <ame22001@byui.edu>
2023-04-20 16:09:04 +0000

    prep12

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your grade percentage 0-100: ");
+        int score = int.Parse(Console.ReadLine());
+
+        string letter;
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+            Console.WriteLine("Your Letter grade is " + letter + ".");
+        {
+    }
     }
 }
