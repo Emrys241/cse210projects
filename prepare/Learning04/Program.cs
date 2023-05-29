bf114e0a4701b2e00bda5e3731e4be81d2043198
Emrys241 <ame22001@byui.edu>
2023-05-29 18:00:21 +0000

    updated

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Bernard Amegah", "Mathematics");
+
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
     }
 }
