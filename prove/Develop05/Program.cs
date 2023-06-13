4cbdeacd559e38aade99d5ad0961db479a2499bc
Emrys241 <ame22001@byui.edu>
2023-06-13 11:28:46 +0000

    updated

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -8,6 +8,7 @@ namespace Develop05
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Develop05 World!");
             Console.WriteLine("Welcome to Goal Tracker!");
 
             while (true)
