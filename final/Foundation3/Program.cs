15658f684fc61457badeacf907030894a946fd6d
Emrys241 <ame22001@byui.edu>
2023-07-13 13:25:35 +0000

    updated date

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -8,13 +8,13 @@ class Program
 
         // Creating instances of different event types
         Lecture lecture = new Lecture("Introduction to Programming", "Learn the basics of programming",
-                                      "2023-07-10", "10:00 AM", "123 Main Street", "John Doe", 50);
+                                      "2023-07-20", "10:00 AM", "123 Main Street", "Bernard Amegah", 50);
 
         Reception reception = new Reception("Company Networking Event", "Network with professionals",
-                                            "2023-07-15", "6:00 PM", "456 Elm Street", "example@example.com", true, false);
+                                            "2023-07-25", "6:00 PM", "456 Elm Street", "ame22001@byui.edu", true, false);
 
         OutdoorGathering gathering = new OutdoorGathering("Summer BBQ", "Enjoy delicious food and games",
-                                                          "2023-07-20", "12:00 PM", "789 Oak Street", "Sunny");
+                                                          "2023-07-30", "12:00 PM", "789 Oak Street", "Sunny");
 
         // Displaying event details
         Console.WriteLine("Lecture:");
